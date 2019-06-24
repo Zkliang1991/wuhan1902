03d223816d1be7f7afd27dce257ab016ae45397f
awslkl <283321992@qq.com>
2019-06-24 19:38:31 +0800

    koulei

---
+++ vue/寇雷/src/components/head.vue
@@ -0,0 +1,26 @@
+<template>
+    <van-nav-bar fixed v-bind:title="title" :left-text="back" :left-arrow="Boolean(left)" @click-left="onClickLeft" @click-right="onClickRight">
+        <van-icon name="search" slot="right" v-if="hide"/>
+    </van-nav-bar>
+</template>
+
+
+<script>
+export default {
+    props:["title","back","left","hide"],
+    methods:{
+        onClickLeft(){
+            history.go(-1);
+        },
+        onClickRight(){
+            this.$router.push({name:"search"});
+        }
+    }
+}
+</script>
+
+<style scoped>
+    img{
+        width: 34px !important;
+    }
+</style>
